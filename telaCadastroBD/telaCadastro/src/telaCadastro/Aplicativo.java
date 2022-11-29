2361991c9584790bc3fa1443e14e6d96a6c39c5a
imaterialtree <119423433+imaterialtree@users.noreply.github.com>
2022-11-29 11:47:10 -0300

    Add files via upload

---
+++ telaCadastroBD/telaCadastro/src/telaCadastro/Aplicativo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package telaCadastro;
+
+import view.FrmMenuPrincipal;
+
+public class Aplicativo {
+
+    public static void main(String[] args) {
+        FrmMenuPrincipal mainMenu = new FrmMenuPrincipal();
+        mainMenu.setVisible(true);
+    }
+    
+}
